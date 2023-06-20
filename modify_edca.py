d8b1d732d168b06083066df247d80083e0f5843d
QianrenLi <908559578@qq.com>
2023-06-20 22:14:23 +0800

    setup for more device

--- modify_edca.py
+++ modify_edca.py
@@ -10,11 +10,11 @@ def main(args):
     if args.reset:
         try:
             with MmapContext() as ctx:
-                if args.realtek:
-                    set_tx_params(ctx, [0], 2, 2, 3 , 1)
-                    set_tx_params(ctx, [1], 2, 3, 4 , 1)
-                    set_tx_params(ctx, [2], 3, 4, 10, 1)
-                    set_tx_params(ctx, [3], 7, 4, 10, 1)
+                if args.ind > 0:
+                    set_tx_params(ctx, [0], 2, 2, 3 , args.ind)
+                    set_tx_params(ctx, [1], 2, 3, 4 , args.ind)
+                    set_tx_params(ctx, [2], 3, 4, 10, args.ind)
+                    set_tx_params(ctx, [3], 7, 4, 10, args.ind)
                 else:
                     reset_tx_params(ctx)
                 print("reset")
@@ -23,8 +23,8 @@ def main(args):
     else:
         try:
             with MmapContext() as ctx:
-                if args.realtek:
-                    set_tx_params(ctx, [args.ac], args.aifs, log2(args.cw_min), log2(args.cw_max), 1)
+                if args.ind > 0:
+                    set_tx_params(ctx, [args.ac], args.aifs, log2(args.cw_min), log2(args.cw_max), args.ind)
                     print("Set ac%d: cw_min=%d, cw_max=%d, aifs=%d" % (args.ac, log2(args.cw_min), log2(args.cw_max), args.aifs))
                 else:
                     set_tx_params(ctx, [args.ac], args.aifs, args.cw_min, args.cw_max)
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     parser.add_argument("--cw-min", type=int, help="cw-min", default=-1)
     parser.add_argument("--cw-max", type=int, help="cw-max", default=-1)
     parser.add_argument("--aifs", type=int, help="aifs", default=-1)
-    parser.add_argument("--realtek", action="store_true", help="set realtime edca parameters")
+    parser.add_argument("--ind", type=int, help="index of wifi card", default=0)
 
     args = parser.parse_args()
     main(args)
